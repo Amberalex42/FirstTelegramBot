e20a301dff7a0ebdb85c63bd02c207ff059ee3c5
Aleksey Malkov <contramoty@gmail.com>
2025-02-26 15:50:30 +0200

    Commit with requests

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-print('New local changes')
-print('Another changes')
+import requests
+
+api_url = 'http://api.open-notify.org/iss-now.json'
+response = requests.get(api_url)
+if response.status_code == 200:
+    print(response.text)
+else:
+    print(response.status_code)
